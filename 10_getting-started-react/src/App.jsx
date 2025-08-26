1f10b866c271cc247666e0945146685e1e9e3d34
rshdgvn <rasheedgavinesponga@gmail.com>
2025-08-26 19:40:48 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>About me</h1>
+        <p>Name: Rasheed Gavin M. Esponga</p>
+        <p>Course/Year: BS Information Technology 3rd Year</p>
+        <p> Fun Fact: I debug at 3 AM and somehow it works..</p>
+      </div>
+      <div>
+        <h1>Why I want to learn React</h1>
+        <p>
+          I want to learn React because it lets me build fast, dynamic, and
+          modern web apps efficiently, while giving me the skills to work on
+          real-world projects and grow as a front-end developer
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
