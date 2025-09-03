755cb1a0c4af95b39f029fbd07da36b43f359842
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-03 20:19:25 +0800

    Add essential features activity

---
+++ 11_javascript-refresher/08_essential_features.js
@@ -0,0 +1,13 @@
+const hobbies = ['tulog', 'idlip', 'tulala']
+hobbies.map(hobby => console.log(hobby))
+
+
+const student = {
+  name: 'rshdgvn',
+  age: 20
+}
+const {name, age} = student
+
+
+const arr = [1, 2, 3]
+const copy = [...arr, 4, 5]
