9cae99875a0d6353ea6b1647668ec38f0b28459e
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-03 19:14:30 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,18 @@
+const name = "rshdgvn"
+const age = 20
+const isStudent = true
+
+console.log(name, age, isStudent, typeof name, typeof age, typeof isStudent)
+
+let x = 1
+let y = 2
+
+const add = x + y
+const subtract = x - y
+const multiply = x * y
+const divide = x / y
+
+console.log("5" == 5);   
+console.log("5" === 5);  
+
+
