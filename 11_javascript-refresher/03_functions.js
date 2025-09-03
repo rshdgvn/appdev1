3786576c8c26fe154b8fc4220519f0f48c99d13c
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-03 19:19:00 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_functions.js
@@ -0,0 +1,14 @@
+function greet(name) {
+  return `Hello ${name}`
+}
+
+const square = (num) => {
+  return num * num
+}
+
+const calculator = (a, b) => {
+  sum = a + b
+  product = a * b
+
+  return a, b
+}
