e2a1753e2fb37bdcdd631bdd6227d17ccad42f3f
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-03 19:26:12 +0800

    Add objects activity

---
+++ 11_javascript-refresher/04_objects.js
@@ -0,0 +1,11 @@
+const aboutMe = {
+  name: 'rshdgvn',
+  age: 20,
+  course: 'BSIS'
+}
+
+aboutMe.introduce = () => {
+  console.log(`Hi, my name is ${aboutMe.name} and I am ${aboutMe.age} years old.`);
+}
+
+aboutMe.hobby = "sleeping";
