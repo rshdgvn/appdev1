0be048f1679b8f62e79c7b051e9026513bd19e22
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-03 19:45:30 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,21 @@
+const grade = 5
+
+if (grade >= 90) {
+  console.log('A');
+} else if (grade >= 80) {
+  console.log('B');
+} else if (grade >= 70) {
+  console.log('C');
+} else {
+  console.log('F');
+}
+
+for (let i = 1; i <= grade; i++) {
+  console.log(i)
+}
+
+let i = 1
+while (i <= 3) {
+  console.log("Hello");
+  i++
+}
