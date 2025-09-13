049b91e13c0a9b4c968c5366083492a0c46874c4
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-13 10:21:07 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css'
+
+function Component() {
+  return <h1>This is a component</h1>;
+}
+
+function App() {
+  return (
+    <>
+      <Component />
+    </>
+  );
+}
+
+export default App;
