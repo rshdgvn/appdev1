e95b3969c2caf0e717a5b8428a31ea028210a1bc
rshdgvn <rasheedgavinesponga@gmail.com>
2025-09-13 10:42:18 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import './App.css'
+import React, {useState} from "react";
+import "./App.css";
 
 function Component() {
   return <h1>This is a component</h1>;
@@ -7,32 +7,46 @@ function Component() {
 
 function App() {
   const headerStyles = {
-    color: 'red'
-  }
+    color: "red",
+  };
 
-  const value = true
+  // const value = true
+  const [value, setValue] = useState(true);
+
+  const handleDate = () => {
+    setValue(!value);
+  };
+
   let data;
 
   if (value) {
-    data = "I'm real"
+    data = "I'm real";
   } else {
-    data = "I'm fake"
+    data = "I'm fake";
   }
- 
+
   const infos = [
-    {name: 'rshdgvn', age: 20},
-    {name: 'vincent', age: 22},
-    {name: 'ryan', age: 22},
-  ]
+    { name: "rshdgvn", age: 20 },
+    { name: "vincent", age: 22 },
+    { name: "ryan", age: 22 },
+  ];
+
+  const handleEvents = () => {
+    console.log("This is an example of handling events");
+  };
 
   return (
     <>
       <Component />
       <h1 style={headerStyles}>I am writing in markup with JSX</h1>
       <p>{data}</p>
+      <button onClick={handleDate}>Change me</button>
       <ul>
-        {infos.map((info)=><li>{`Hello i am ${info.name} and I am ${info.age} years old`}</li>)}
+        {infos.map((info) => (
+          <li>{`Hello i am ${info.name} and I am ${info.age} years old`}</li>
+        ))}
       </ul>
+      <button onClick={handleEvents}>Click</button>
     </>
   );
 }
