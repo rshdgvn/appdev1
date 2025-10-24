1e5f783bf1f15c9f62e6e5082b1f2c5fcce5c4d2
rshdgvn <rasheedgavinesponga@gmail.com>
2025-10-25 04:27:43 +0800

    Done with how to write your first React component

---
+++ react-describing-ui/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
