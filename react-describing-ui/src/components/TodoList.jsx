2b45660a5ec1d4040f145d1b5d1dcd3852a6e0a1
rshdgvn <rasheedgavinesponga@gmail.com>
2025-10-25 04:43:20 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

--- react-describing-ui/src/components/TodoList.jsx
+++ react-describing-ui/src/components/TodoList.jsx
@@ -1,38 +1,27 @@
-import React from "react"
+import React from "react";
 
-// export default function TodoList() {
-//   return (
-//     // This doesn't quite work!
-//     <h1>Hedy Lamarr's Todos</h1>
-//     <img
-//       src="https://i.imgur.com/yXOvdOSs.jpg"
-//       alt="Hedy Lamarr"
-//       class="photo"
-//     >
-//     <ul>
-//       <li>Invent new traffic lights
-//       <li>Rehearse a movie scene
-//       <li>Improve spectrum technology
-//     </ul>
-//   );
-// }
+const person = {
+  name: "Gregorio Y. Zara",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
 
 export default function TodoList() {
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
       />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    </>
+    </div>
   );
 }
-
-
