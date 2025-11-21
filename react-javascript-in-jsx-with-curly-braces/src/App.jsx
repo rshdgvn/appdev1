ee74a49ef494fb59c615aaaf78a2726d23422fa3
rshdgvn <rasheedgavinesponga@gmail.com>
2025-11-21 09:46:36 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Avatar from "./components/Avatar";
+import TodoList from "./components/ToDoList";
 
 function App() {
   return (
     <>
       <Avatar />
+      <TodoList />
     </>
   );
 }
